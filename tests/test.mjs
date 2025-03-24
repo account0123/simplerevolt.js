d9609650e141a26195a157a6c194337f151b9335
Ian Parra <ianparra200@gmail.com>
2025-03-24 16:44:24 -0300

    fix: show error handling in examples

--- tests/test.mjs
+++ tests/test.mjs
@@ -1,11 +1,21 @@
 import "dotenv/config";
 import { Client } from "../lib/esm/index.js";
 
-const client = new Client({debug: true});
+const client = new Client({ debug: true });
 
 client.on("ready", () => console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`));
 
-client.on("messageCreate", (message) => console.log(`${message.author.username}#${message.author.discriminator}: ${message.content}`));
+client.on("messageCreate", async (message) => {
+  console.log(`${message.author.username}#${message.author.discriminator}: ${message.content}`);
+
+  try {
+    if (message.content == "!ping") {
+      await message.reply("Pong!");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 client.on("disconnected", () => console.log("Disconnected"));
 
