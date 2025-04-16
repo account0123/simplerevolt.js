09e176f7a3ebc8386a9692a269e14a123fb2ca44
Ian Parra <ianparra200@gmail.com>
2025-04-15 21:58:51 -0400

    feat: server fetch invites

--- src/models/Server.ts
+++ src/models/Server.ts
@@ -13,6 +13,7 @@ import { PermissionsBitField } from "../permissions/PermissionsBitField.js";
 import { ALLOW_IN_TIMEOUT, Permission, PermissionOverrides } from "../permissions/index.js";
 import { BitField } from "../utils/BitField.js";
 import { ServerInviteCollection } from "../collections/InviteCollection.js";
+import { ServerInviteData } from "./Invite.js";
 
 export class Server extends Base {
   // @ts-ignore unused
@@ -96,6 +97,15 @@ export class Server extends Base {
           break;
       }
     }
+  }
+
+  /**
+   * Fetch all server invites.
+   * @throws RevoltAPIError
+   */
+  async fetchInvites() {
+    const invites = (await this.client.api.get(`/servers/${this.id as ""}/invites`)) as ServerInviteData[]; // Assuming the API filters server invites
+    return invites.map((invite) => this.invites.create(invite));
   }
 
   /**
