fed5daaa99bf3d622b42fd617eeb345aa1798485
Ian Parra <ianparra200@gmail.com>
2025-05-12 19:01:39 -0400

    fmt: write

--- src/collections/ServerMemberCollection.ts
+++ src/collections/ServerMemberCollection.ts
@@ -87,17 +87,17 @@ export class ServerMemberCollection extends CachedCollection<ServerMember> {
 
   /**
    * Timeout a member for a duration since Date.now()
-   * @param member 
+   * @param member
    * @param duration in seconds
    */
   timeoutFor(member: string | ServerMember, duration: number) {
-    const until = Date.now() + (duration * 1_000);
+    const until = Date.now() + duration * 1_000;
     return this.timeoutUntil(member, new Date(until));
   }
 
   /**
    * Timeout a member.
-   * @param member 
+   * @param member
    * @param timeout Date or ISO string
    */
   timeoutUntil(member: string | ServerMember, timeout: Date | string) {
